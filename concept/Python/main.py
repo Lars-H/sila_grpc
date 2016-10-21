1fa81fb48715e1e77c1808f14e27fc2764a98ded
Lars-H <lars.heling@gmx.de>
2016-10-21 11:52:57 +0200

    Implemented first version command line interaction for interacting with the server

--- concept/Python/main.py
+++ concept/Python/main.py
@@ -1,11 +1,46 @@
+import re
+
+# Import the DeviceCLient that provides interface to the grpc service
 from clients import DeviceClient
 
 client = DeviceClient()
-print (client.is_sila())
+
+
+def command(commandStr, *args):
+    """
+    Executes command given by a command string
+    Args:
+        commandStr: String representing the command
+        args: additional arguments, if necessary
+
+    Returns:
+    """
+
+    # IsSiLA Command
+    if re.match(commandStr, "IsSiLA", re.IGNORECASE):
+        print (client.is_sila())
+
+    # Device specific command
+    elif re.match(commandStr, "temperature", re.IGNORECASE):
+        if len(args) == 1:
+            client.get_datastream(int(args[0]))
+        else:
+            print (client.get_datapoint())
+
+    else:
+        print ("Command not found.")
 
 
 try:
     while True:
-        print("Tets")
+        print ("Enter a command: ")
+        inputStr =  raw_input()
+        if re.match(inputStr, "exit", re.IGNORECASE) or re.match(inputStr, "q", re.IGNORECASE):
+            break;
+        inputStr = inputStr.split(" ")
+        if len(inputStr) >= 2:
+            command(inputStr[0], inputStr[1:][0])
+        else:
+            command(commandStr=inputStr[0])
 except KeyboardInterrupt:
     print ("Client disconnected")
